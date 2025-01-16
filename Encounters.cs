9fb3e7b68659c167c8ae5aaa5293b03973fcce66
Farhan Nadaf <farhannadaf06@gmail.com>
2025-01-16 19:41:07 +0530

    Add project files.

---
+++ Encounters.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantacyCode1
+{
+    internal class Encounters
+    {
+        static Random rand = new Random();
+        //Encounter Generic
+
+
+        //Encounters
+        public static void FirstEncounter()
+        {
+            Console.WriteLine("You run behind a tree to take cover and there you find a LONG STICK ");
+            Console.WriteLine("Now you gather up the courage to fight the monster...");
+            Console.ReadKey();
+            Combat(false,"A Spooky Tree",2,6);
+        }
+
+
+        //Encounter tools
+        public static void Combat(bool random, string name, int power, int health)
+        {
+            string n = "";
+            int p = 0;
+            int h = 0;
+            if (random)
+            {
+
+
+            }
+            else 
+            {
+                n = name;
+                p = power;
+                h = health;
+
+            }
+            while (h>0)
+            {
+                Console.Clear();
+                Console.WriteLine(n);
+                Console.WriteLine("Power = "+p +" / health = "+ h);
+
+                Console.WriteLine("==============================");
+                Console.WriteLine("|   (A)ttack      (D)fend    |");
+                Console.WriteLine("|   (R)un         (H)eal     |");
+                Console.WriteLine("==============================");
+                Console.WriteLine("Potions :"+ Program.Player.potion + "  Health :"+ Program.Player.health);
+                string input = Console.ReadLine();
+                if (input.ToLower() == "a" || input.ToLower() == "attack")
+                {
+                    //attack
+                    Console.WriteLine("You charge forward with courage and slash the " + n + ". and he also hits you as u pass near him");
+                    int damage = p - Program.Player.armor;
+                    int attack = rand.Next(0, Program.Player.weaponValue) + rand.Next(1,4);
+                    Console.WriteLine("You lose "+ damage +" Health while you deal "+ attack +" damage to your enemy");
+                    Program.Player.health -= damage;
+                    h -= attack;
+
+                }
+                else if (input.ToLower() == "d" || input.ToLower() == "defend")
+                {
+                    //defend
+                    Console.WriteLine("As the " + n + " dives in to strike you, you form a defensive stance and are able to defend from enemies attack");
+                    int damage = (p/4) - Program.Player.armor;
+                    if (damage < 0)
+                        damage = 0;
+                    int attack = rand.Next(0, Program.Player.weaponValue)/2;
+
+                    Console.WriteLine("You lose " + damage + " Health while you deal " + attack + " damage to your enemy");
+                    Program.Player.health -= damage;
+                    h -= attack;
+                }
+                else if (input.ToLower() == "r" || input.ToLower() == "run")
+                {
+                    //run
+                    if ( rand.Next(0,2) == 0 )
+                    {
+                        Console.WriteLine("As you try to RUN the "+n+" caught  you again.");
+                        int damage = p - Program.Player.armor;
+                        if (damage < 0)
+                            damage = 0;
+                        Console.WriteLine("You lose "+ damage +" Health as you desperately try to escape");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You Outrun the Monster and succesfully escape.. !");
+                        Console.ReadKey();
+                    }
+                }
+                else if (input.ToLower() == "h" || input.ToLower() == "heal")
+                {
+                    //heal
+                    if (Program.Player.potion == 0)
+                    {
+                        Console.WriteLine("You've got no potions noww...GET BACK !!");
+                        int damage = p - Program.Player.armor;
+                        if (damage < 0)
+                            damage = 0;
+
+                        Console.WriteLine("As you were trying to find a potion the " + n + " sneaked up and striked you.");
+                        Console.WriteLine("And you lost " + damage + " Health.");
+                    }
+                    else 
+                    { 
+                        Console.WriteLine("You grabbed a bottle of potion and drank it..");
+                        int potionvalue = 5;
+                        Console.WriteLine("You gain "+potionvalue+" Health..");
+                        Program.Player.health += potionvalue;
+                        Console.WriteLine("As you were busy drinking the potion the "+n+" took the chance and struck.");
+                        int damage = (p - Program.Player.armor) / 2;
+                        if (damage < 0)
+                            damage = 0;
+                        Console.WriteLine("You lost "+ damage +" Health.");
+                    }
+                }
+
+                Console.ReadKey();
+            }
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantacyCode1
+{
+    internal class Program
+    {
+        public static Player Player = new Player();
+        static void Main(string[] args)
+        {
+            Start();
+            Encounters.FirstEncounter();
+        }
+
+        static void Start()
+        {
+            Console.WriteLine("Magical Forest");
+            Console.WriteLine("Name:");
+            Console.ReadLine();
+            Player.name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("You wake up in a magical forest..., you feel dizzy and are having trouble remembering");
+            Console.WriteLine("anything about your past.");
+            if (Player.name == "")
+                Console.WriteLine("You know your name is " + Player.name);
+            else
+                Console.WriteLine("You cant even remember your name.");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("You try to search for anything helpful until you see a pathway leading to something.");
+            Console.WriteLine("you follow the pathway through the dark and tall forest in hopes of getting out but...");
+            Console.ReadKey();
+            Console.WriteLine("There you see a monstrous figure standing before you in the dark.");
+        }
+    }
+}
